bf5afee25996958178b91f91ca894995e79c9968
Joe Maloney <118585970+Peas-N-Carrots@users.noreply.github.com>
2025-11-04 23:12:50 -0500

    organizing player code (not functional rn)

--- player.gd
+++ player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody3D
 
-const ACCEL_SPEED = 0.2
-const DECEL_SPEED = 0.1
+const ACCEL_SPEED = 12
+const DECEL_SPEED = 6
 const RUN_SPEED = 4.0
 const JUMP_VELOCITY = 4.5
 const MOUSE_SENSITIVITY = 0.0015
+const JUMP_BUFFER = 0.5
+const COYOTE_TIME = 0.5
 
 const MAX_WOBBLE = 0.005
 
@@ -12,8 +14,13 @@ const MAX_WOBBLE = 0.005
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-var trip_level : float = 0.0;
-var time := 0.0;
+var trip_level : float = 0.0
+var time := 0.0
+
+var input_dir := Vector2.ZERO
+
+var ground_frame := 0.0
+var jump_frame := 0.0
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -26,26 +33,47 @@ func _input(event):
 
 func _physics_process(delta):
 	time += delta
-	var offset = Vector2(sin(time * 1.53 + 4.8), sin(time * 2.8)) * MAX_WOBBLE * trip_level;
+	get_input()
 	
+	wobble_cam()
+	
+	apply_gravity(delta)
+	get_jump()
+	input_to_velocity(delta)
+	
+	move_and_slide()
+
+func get_jump_frame() -> bool:
+	return time - jump_frame < JUMP_BUFFER
+
+func get_ground_frame() -> bool:
+	return time - ground_frame < COYOTE_TIME
+
+func wobble_cam() -> void:
+	var offset = Vector2(sin(time * 1.53 + 4.8), sin(time * 2.8)) * MAX_WOBBLE * trip_level
 	rotate_y(offset.y)
 	camera.rotate_x(offset.x)
 	camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
-	
+
+func get_input() -> void:
+	input_dir = Input.get_vector("Left", "Right", "Forwards", "Backwards")
+	if Input.is_action_just_pressed("Jump"): jump_frame = time
+	if is_on_floor(): ground_frame = time
+
+func get_jump() -> void:
+	if get_jump_frame() and get_ground_frame():
+		velocity.y = JUMP_VELOCITY
+
+func apply_gravity(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	
-	var input_dir = Input.get_vector("Left", "Right", "Forwards", "Backwards")
+
+func input_to_velocity(delta: float) -> void:
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() #+ (MAX_WOBBLE * trip_level * offset)
 	
 	if direction:
-		velocity.x = move_toward(velocity.x, direction.x * RUN_SPEED, ACCEL_SPEED)
-		velocity.z = move_toward(velocity.z, direction.z * RUN_SPEED, ACCEL_SPEED)
+		velocity.x = move_toward(velocity.x, direction.x * RUN_SPEED, ACCEL_SPEED * delta)
+		velocity.z = move_toward(velocity.z, direction.z * RUN_SPEED, ACCEL_SPEED * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, DECEL_SPEED)
-		velocity.z = move_toward(velocity.z, 0, DECEL_SPEED)
-	
-	move_and_slide()
+		velocity.x = move_toward(velocity.x, 0, DECEL_SPEED * delta)
+		velocity.z = move_toward(velocity.z, 0, DECEL_SPEED * delta)
