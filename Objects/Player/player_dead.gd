732777c2ebc575acd07c33379db919f13eef0d3a
Joe Maloney <118585970+Peas-N-Carrots@users.noreply.github.com>
2025-11-12 18:57:07 -0500

    restore sfx

---
+++ Objects/Player/player_dead.gd
@@ -0,0 +1,10 @@
+extends RigidBody3D
+
+const MIN_VEL = 2.0
+
+var p_vel = Vector3.ZERO
+
+func _physics_process(delta):
+	if (linear_velocity - p_vel).length() >= MIN_VEL:
+		$AudioImpact.play()
+	p_vel = linear_velocity
